6309041bdc1620a8c9553fe735dd14d28eb5936b
brentzucker <brentzucker@gmail.com>
2015-10-27 13:19:37 -0400

    Used fragment Arguments to pass to activity to maintain fragment independence

--- CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeActivity.java
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeActivity.java
@@ -8,11 +8,13 @@ import java.util.UUID;
 
 public class CrimeActivity extends SingleFragmentActivity {
 
-    public static final String EXTRA_CRIME_ID = "com.bignerdranch.android.criminalintent.crime_id";
+    private static final String EXTRA_CRIME_ID = "com.bignerdranch.android.criminalintent.crime_id";
 
     @Override
     protected Fragment createFragment() {
-        return new CrimeFragment();
+        UUID crimeId = (UUID) getIntent()
+                .getSerializableExtra(EXTRA_CRIME_ID);
+        return CrimeFragment.newInstance(crimeId);
     }
 
     public static Intent newIntent(Context packageContext, UUID crimeId) {
