4bbe878f9906a4a94da3784d6aae19688b229b4e
brentzucker <brentzucker@gmail.com>
2015-10-28 23:05:26 -0400

    Created CrimePagerActivity
    - will create and manage the ViewPager
    - created activity_crime_pager layout

---
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimePagerActivity.java
@@ -0,0 +1,16 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Created by bvz on 10/28/15.
+ */
+public class CrimePagerActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crime_pager)
+    }
+}
