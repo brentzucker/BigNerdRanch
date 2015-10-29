681fe2edbf9f28c43781afdc046a6719640b34ce
brentzucker <brentzucker@gmail.com>
2015-10-29 08:58:15 -0400

    Integrated CrimePageActivity
    - Replace CrimeActivity with CrimePageActivity (ViewPager)

--- CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimePagerActivity.java
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimePagerActivity.java
@@ -1,5 +1,7 @@
 package com.bignerdranch.android.criminalintent;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
@@ -8,19 +10,30 @@ import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 
 import java.util.List;
+import java.util.UUID;
 
 /**
  * Created by bvz on 10/28/15.
  */
 public class CrimePagerActivity extends FragmentActivity {
 
+    private static final String EXTRA_CRIME_ID = "com.bignerdranch.android.criminalintent.crime_id";
+
     private ViewPager mViewPager;
     private List<Crime> mCrimes;
+
+    public static Intent newIntent(Context packageContext, UUID crimeId) {
+        Intent intent = new Intent(packageContext, CrimePagerActivity.class);
+        intent.putExtra(EXTRA_CRIME_ID, crimeId);
+        return intent;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime_pager);
+
+        UUID crimeID = (UUID) getIntent().getSerializableExtra(EXTRA_CRIME_ID);
 
         // Find ViewPager
         mViewPager = (ViewPager) findViewById(R.id.activity_crime_pager_view_pager);
@@ -42,6 +55,4 @@ public class CrimePagerActivity extends FragmentActivity {
             }
         });
     }
-
-
 }
