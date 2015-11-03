c8f0e5bed5708816024a0b5c3e0bbb05ff50c39d
brentzucker <brentzucker@gmail.com>
2015-11-02 20:45:37 -0500

    Set Dialogs Content to DatePicker

--- CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/DatePickerFragment.java
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/DatePickerFragment.java
@@ -3,6 +3,8 @@ package com.bignerdranch.android.criminalintent;
 import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.view.View;
 
 /**
  * Created by bvz on 10/29/15.
@@ -12,7 +14,11 @@ public class DatePickerFragment extends android.support.v4.app.DialogFragment {
     // FragmentManager of Hosting activity calls this method
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        View v = LayoutInflater.from(getActivity()).inflate(R.layout.dialog_date, null);
+
         return new AlertDialog.Builder(getActivity())
+                .setView(v)
                 .setTitle(R.string.date_picker_title)
                 .setPositiveButton(android.R.string.ok, null)
                 .create();
