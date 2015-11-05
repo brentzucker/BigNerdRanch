291bbd57f21129147549628b13eb7fdbca796588
brentzucker <brentzucker@gmail.com>
2015-11-04 19:46:10 -0500

    Created SQLiteOpenHelper class

---
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeBaseHelper.java
@@ -0,0 +1,32 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by bvz on 11/4/15.
+ */
+/* This class checks to see if db exists
+ *  if not creates tables and initial data
+ *  if does open and checks version
+ *  updates old version
+ */
+public class CrimeBaseHelper extends SQLiteOpenHelper {
+    private static final int VERSION = 1;
+    private static final String DATABASE_NAME = "crimeBase.db";
+
+    public CrimeBaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
