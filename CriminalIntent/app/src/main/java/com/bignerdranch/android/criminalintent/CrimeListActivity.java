8fb607c86c027b1ad2b6806711fbb434ecbc68d0
brentzucker <brentzucker@gmail.com>
2015-11-06 18:18:20 -0500

    use alias resource to display different layout if tablet/phone

--- CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListActivity.java
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListActivity.java
@@ -14,6 +14,6 @@ public class CrimeListActivity extends SingleFragmentActivity {
 
     @Override
     protected int getLayoutResId() {
-        return R.layout.activity_twopane;
+        return R.layout.activity_masterdetail;
     }
 }
