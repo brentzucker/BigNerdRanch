23f9e91be28653862a9cd86df221377b31184269
brentzucker <brentzucker@gmail.com>
2015-11-08 14:39:52 -0500

    Use asset manager to load sound files

--- BeatBox/app/src/main/java/com/bignerdranch/android/beatbox/BeatBoxFragment.java
+++ BeatBox/app/src/main/java/com/bignerdranch/android/beatbox/BeatBoxFragment.java
@@ -14,8 +14,17 @@ import android.widget.Button;
  */
 public class BeatBoxFragment extends Fragment {
 
+    private BeatBox mBeatBox;
+
     public static BeatBoxFragment newInstance() {
         return new BeatBoxFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mBeatBox = new BeatBox(getActivity());
     }
 
     @Override
