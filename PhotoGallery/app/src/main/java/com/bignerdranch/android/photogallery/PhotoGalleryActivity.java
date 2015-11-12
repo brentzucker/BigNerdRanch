6df30ca9a1d3816d35ecd03af76875b198cbd467
brentzucker <brentzucker@gmail.com>
2015-11-11 23:22:43 -0500

    Created PhotoGallery

---
+++ PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryActivity.java
@@ -0,0 +1,11 @@
+package com.bignerdranch.android.photogallery;
+
+import android.support.v4.app.Fragment;
+
+public class PhotoGalleryActivity extends SingleFragmentActivity {
+
+    @Override
+    public Fragment createFragment() {
+        return PhotoGalleryFragment.newInstance();
+    }
+}
