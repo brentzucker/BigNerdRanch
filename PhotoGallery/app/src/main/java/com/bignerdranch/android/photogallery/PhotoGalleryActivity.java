99e50837bef96d0064f65653cc15ecae5f30325f
brentzucker <brentzucker@gmail.com>
2015-11-15 12:52:39 -0500

    Notifications
    — added a notification to poll service

--- PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryActivity.java
+++ PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryActivity.java
@@ -1,8 +1,14 @@
 package com.bignerdranch.android.photogallery;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 
 public class PhotoGalleryActivity extends SingleFragmentActivity {
+
+    public static Intent newIntent(Context context) {
+        return new Intent(context, PhotoGalleryActivity.class);
+    }
 
     @Override
     public Fragment createFragment() {
--- PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/PollService.java
+++ PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/PollService.java
@@ -2,11 +2,15 @@ package com.bignerdranch.android.photogallery;
 
 import android.app.AlarmManager;
 import android.app.IntentService;
+import android.app.Notification;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.net.ConnectivityManager;
 import android.os.SystemClock;
+import android.support.v4.app.NotificationManagerCompat;
+import android.support.v7.app.NotificationCompat;
 import android.util.Log;
 
 import java.util.List;
@@ -17,7 +21,7 @@ import java.util.List;
 public class PollService extends IntentService {
 
     private static final String TAG = "PollService";
-    private static final int POLL_INTERVAL = 1000 * 60; // 60 seconds
+    private static final long POLL_INTERVAL = AlarmManager.INTERVAL_FIFTEEN_MINUTES;
 
     public static Intent newIntent(Context context) {
         Log.i(TAG, "newIntent");
@@ -73,6 +77,23 @@ public class PollService extends IntentService {
         } else {
             Log.i(TAG, "Got a new result: " + resultId);
         }
+
+        Resources resources = getResources();
+        Intent i = PhotoGalleryActivity.newIntent(this);
+        PendingIntent pi = PendingIntent.getActivity(this, 0, i, 0);
+
+        // Create notification
+        Notification notification = new NotificationCompat.Builder(this)
+                .setTicker(resources.getString(R.string.new_pictures_title))
+                .setSmallIcon(android.R.drawable.ic_menu_report_image)
+                .setContentTitle(resources.getString(R.string.new_pictures_title))
+                .setContentText(resources.getString(R.string.new_pictures_text))
+                .setContentIntent(pi)
+                .setAutoCancel(true) // removes notification from drawer
+                .build();
+
+        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(this);
+        notificationManagerCompat.notify(0, notification);
 
         QueryPreferences.setPrefLastResultId(this, resultId);
 
