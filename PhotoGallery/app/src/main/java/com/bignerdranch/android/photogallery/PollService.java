c4ce3b3c444f52a0801545ed11085dbbf856fa4b
brentzucker <brentzucker@gmail.com>
2015-11-15 09:54:42 -0500

    safe background networking to poll flickr in background
    - check if connected to network
    - modified manifest permssions

--- PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/PollService.java
+++ PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/PollService.java
@@ -3,6 +3,7 @@ package com.bignerdranch.android.photogallery;
 import android.app.IntentService;
 import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
 import android.util.Log;
 
 /**
@@ -23,5 +24,19 @@ public class PollService extends IntentService {
     @Override
     protected void onHandleIntent(Intent intent) {
         Log.i(TAG, "Received an intent: " + intent);
+
+        // Check that user did not turn off networking in background applications
+        if (!isNetworkAvailableAndConnected()) {
+            return;
+        }
+    }
+
+    private boolean isNetworkAvailableAndConnected() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+
+        boolean isNetworkAvailable = cm.getActiveNetworkInfo() != null;
+        boolean isNetworkConnected = isNetworkAvailable && cm.getActiveNetworkInfo();
+
+        return isNetworkConnected;
     }
 }
