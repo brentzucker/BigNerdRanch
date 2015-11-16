5822729b72fabed5b089ff19e2cc9c0c22e01b4d
brentzucker <brentzucker@gmail.com>
2015-11-16 13:20:07 -0500

    Receiving a System Broadcast: Waking up on Boot: Creating and Registering a standalone receiver
    - modifed manifest to receive boot completed

---
+++ PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/StartupReceiver.java
@@ -0,0 +1,20 @@
+package com.bignerdranch.android.photogallery;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+/**
+ * Created by bvz on 11/16/15.
+ */
+public class StartupReceiver extends BroadcastReceiver {
+
+    private static final String TAG = "StartupReceiver";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        Log.i(TAG, "Received broadcast intent: " + intent.getAction());
+    }
+}
