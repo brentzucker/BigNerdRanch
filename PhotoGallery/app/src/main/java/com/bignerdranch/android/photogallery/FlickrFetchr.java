6a404c493f33ed3a2225d7abab7d76f56cb81f73
brentzucker <brentzucker@gmail.com>
2015-11-24 09:20:23 -0500

    Get extra flickr data to browse web

--- PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/FlickrFetchr.java
+++ PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/FlickrFetchr.java
@@ -115,6 +115,7 @@ public class FlickrFetchr {
             }
 
             item.setUrl(photoJsonObject.getString("url_s"));
+            item.setOwner(photoJsonObject.getString("owner"));
             items.add(item);
         }
     }
--- PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/GalleryItem.java
+++ PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/GalleryItem.java
@@ -1,5 +1,7 @@
 package com.bignerdranch.android.photogallery;
 
+import android.net.Uri;
+
 /**
  * Created by bvz on 11/12/15.
  */
@@ -7,11 +9,7 @@ public class GalleryItem {
     private String mCaption;
     private String mId;
     private String mUrl;
-
-    @Override
-    public String toString() {
-        return mCaption;
-    }
+    private String mOwner;
 
     public String getCaption() {
         return mCaption;
@@ -35,5 +33,26 @@ public class GalleryItem {
 
     public void setUrl(String url) {
         mUrl = url;
+    }
+
+    public String getOwner() {
+        return mOwner;
+    }
+
+    public void setOwner(String owner) {
+        mOwner = owner;
+    }
+
+    public Uri getPhotoPageUri() {
+        return Uri.parse("http://www.flickr.com/photos/")
+                .buildUpon()
+                .appendPath(mOwner)
+                .appendPath(mId)
+                .build();
+    }
+
+    @Override
+    public String toString() {
+        return mCaption;
     }
 }
