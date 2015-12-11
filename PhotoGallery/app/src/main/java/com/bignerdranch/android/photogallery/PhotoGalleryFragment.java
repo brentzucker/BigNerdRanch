9bd328990d2f7bbe3ee16f097268499f287715e1
brentzucker <brentzucker@gmail.com>
2015-12-11 11:52:02 -0500

    Launches WebView instead: launches empty fragment
    an activity with a fragment containing a web view

--- PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.java
+++ PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.java
@@ -174,7 +174,7 @@ public class PhotoGalleryFragment extends VisibleFragment {
 
         @Override
         public void onClick(View v) {
-            Intent i = new Intent(Intent.ACTION_VIEW, mGalleryItem.getPhotoPageUri());
+            Intent i = PhotoPageActivity.newIntent(getActivity(), mGalleryItem.getPhotoPageUri());
             startActivity(i);
         }
     }
---
+++ PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/PhotoPageFragment.java
@@ -0,0 +1,43 @@
+package com.bignerdranch.android.photogallery;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+
+/**
+ * Created by bvz on 12/11/15.
+ */
+public class PhotoPageFragment extends VisibleFragment {
+    private static final String ARG_URI = "photo_page_url";
+
+    private Uri mUri;
+
+    private WebView mWebView;
+
+    public static PhotoPageFragment newInstance(Uri uri) {
+        Bundle args = new Bundle();
+        args.putParcelable(ARG_URI, uri);
+
+        PhotoPageFragment fragment = new PhotoPageFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mUri = getArguments().getParcelable(ARG_URI);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_photo_page, container, false);
+
+        mWebView = (WebView) v.findViewById(R.id.fragment_photo_page_web_view);
+        return v;
+    }
+}
