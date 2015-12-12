93d8ac24c3b6e36a87d31e2532a704c0fcf56b68
brentzucker <brentzucker@gmail.com>
2015-12-12 00:12:38 -0500

    Tracking across motion events
    created box object

--- DragAndDraw/app/src/main/java/com/bignerdranch/android/draganddraw/BoxDrawingView.java
+++ DragAndDraw/app/src/main/java/com/bignerdranch/android/draganddraw/BoxDrawingView.java
@@ -7,11 +7,17 @@ import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by bvz on 12/11/15.
  */
 public class BoxDrawingView extends View {
     private static final String TAG = "BoxDrawingView";
+
+    private Box mCurrentBox;
+    private List<Box> mBoxen = new ArrayList<>();
 
     // Used when creating the view in code
     public BoxDrawingView(Context context) {
@@ -31,15 +37,24 @@ public class BoxDrawingView extends View {
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
                 action = "ACTION_DOWN";
+                // Reset drawing state
+                mCurrentBox = new Box(current);
+                mBoxen.add(mCurrentBox);
                 break;
             case MotionEvent.ACTION_MOVE:
                 action = "ACTION_MOVE";
+                if (mCurrentBox != null) {
+                    mCurrentBox.setCurrent(current);
+                    invalidate();
+                }
                 break;
             case MotionEvent.ACTION_UP:
                 action = "ACTION_UP";
+                mCurrentBox = null;
                 break;
             case MotionEvent.ACTION_CANCEL:
                 action = "ACTION_CANCEL";
+                mCurrentBox = null;
                 break;
         }
         Log.i(TAG, action + " at x=" + current.x + ", y=" + current.y);
