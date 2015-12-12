b9874c000bb4b22aae37553481da574a7c0ad77d
brentzucker <brentzucker@gmail.com>
2015-12-12 00:07:53 -0500

    Handling Touch Events
    logs touch event coordinates

--- DragAndDraw/app/src/main/java/com/bignerdranch/android/draganddraw/BoxDrawingView.java
+++ DragAndDraw/app/src/main/java/com/bignerdranch/android/draganddraw/BoxDrawingView.java
@@ -1,13 +1,17 @@
 package com.bignerdranch.android.draganddraw;
 
 import android.content.Context;
+import android.graphics.PointF;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 
 /**
  * Created by bvz on 12/11/15.
  */
 public class BoxDrawingView extends View {
+    private static final String TAG = "BoxDrawingView";
 
     // Used when creating the view in code
     public BoxDrawingView(Context context) {
@@ -17,5 +21,28 @@ public class BoxDrawingView extends View {
     // Used when inflating the view from XML
     public BoxDrawingView(Context context, AttributeSet attrs) {
         super(context, attrs);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        PointF current = new PointF(event.getX(), event.getY());
+        String action = "";
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                action = "ACTION_DOWN";
+                break;
+            case MotionEvent.ACTION_MOVE:
+                action = "ACTION_MOVE";
+                break;
+            case MotionEvent.ACTION_UP:
+                action = "ACTION_UP";
+                break;
+            case MotionEvent.ACTION_CANCEL:
+                action = "ACTION_CANCEL";
+                break;
+        }
+        Log.i(TAG, action + " at x=" + current.x + ", y=" + current.y);
+        return true;
     }
 }
