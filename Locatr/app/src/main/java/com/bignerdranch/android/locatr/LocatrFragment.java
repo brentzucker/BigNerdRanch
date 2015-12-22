2556b60fc1741c4fdaa07553a203ca0ecf4e0395
brentzucker <brentzucker@gmail.com>
2015-12-22 18:27:59 -0500

    Find and Display an Image

--- Locatr/app/src/main/java/com/bignerdranch/android/locatr/LocatrFragment.java
+++ Locatr/app/src/main/java/com/bignerdranch/android/locatr/LocatrFragment.java
@@ -1,6 +1,9 @@
 package com.bignerdranch.android.locatr;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.location.Location;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
@@ -16,6 +19,9 @@ import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
+
+import java.io.IOException;
+import java.util.List;
 
 /**
  * Created by bvz on 12/22/15.
@@ -101,12 +107,43 @@ public class LocatrFragment extends Fragment {
         request.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
         request.setNumUpdates(1);
         request.setInterval(0);
-        LocationServices.FusedLocationApi
-                .requestLocationUpdates(mClient, request, new LocationListener() {
-                    @Override
-                    public void onLocationChanged(Location location) {
-                        Log.i(TAG, "Got a fix: " + location);
-                    }
-                })
+        LocationServices.FusedLocationApi.requestLocationUpdates(mClient, request, new LocationListener() {
+                @Override
+                public void onLocationChanged(Location location) {
+                    Log.i(TAG, "Got a fix: " + location);
+                    new SearchTask().execute(location);
+                }
+            })
+    }
+
+    private class SearchTask extends AsyncTask<Location, Void, Void> {
+
+        private GalleryItem mGalleryItem;
+        private Bitmap mBitmap;
+
+        @Override
+        protected Void doInBackground(Location... params) {
+            FlickrFetchr fetchr = new FlickrFetchr();
+            List<GalleryItem> items = fetchr.searchPhotos(params[0]);
+
+            if (items.size() == 0) {
+                return null;
+            }
+
+            mGalleryItem = items.get(0);
+
+            try {
+                byte[] bytes = fetchr.getUrlBytes(mGalleryItem.getUrl());
+                mBitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+            } catch (IOException ioe) {
+                Log.i(TAG, "Unable to download bitmap", ioe);
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecutre(Void result) {
+            mImageView.setImageBitmap(mBitmap);
+        }
     }
 }
